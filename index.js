01878bd977c0e814e2004e5b2df4722b334b5869
Jai Dev <47059807+youngsterjaidev@users.noreply.github.com>
2026-03-13 20:49:55 +0530

    Fix typo in process.env logging

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-console.log(proces.env)
+console.log(process.env)
 
 // ==========================================
 // MSSQL DATABASE CONNECTION
